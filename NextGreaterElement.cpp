0b653892755aec11585a66042a8207c0236b5c60
adesh-programmer <81546311+adesh-programmer@users.noreply.github.com>
2021-03-29 09:54:37 +0530

    Update NextGreaterElement.cpp

--- NextGreaterElement.cpp
+++ NextGreaterElement.cpp
@@ -33,5 +33,23 @@ class Solution{
 /* Driver code */
 int main()
 {
-	
+  int t;
+  cin >> t;
+  while(t--)
+  {
+	  int n;
+	  cin >> n;
+	  vector <long long> arr(n);
+	  for(int i = 0; i < n; i++)
+		  cin >> arr[i];
+	  
+	  Solution obj;
+	  
+	  vector <long long> res  = obj.nextLargerElement(arr, n);
+	  
+	  for(long long i : res)
+		  cout << i << " ";
+	  cout << endl;
+  }
+	return 0;
 }
